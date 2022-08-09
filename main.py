3a4b6226c14f79447ddf3e6566511a2aa0f2741e
Jihong Lee <jihong2jihong@gmail.com>
2022-08-09 15:38:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('sine(x) & cosine(x))
+st.header('sine(x) & cosine(x)')
 
 x = np.arange(0, 9, 0.1)
 df = pd.DataFrame({'x' : x, 'sin_x' : np.sin(x), 'cos_x' : np.cos(x)})
